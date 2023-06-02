f693dfa65e75748ec0bbea0297f1bcee8a57376b
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-06-02 17:34:00 +0900

    Implement Linear Probing Hash Table

--- linearhash.cpp
+++ linearhash.cpp
@@ -3,5 +3,31 @@
 
 int main()
 {
+    //create Hash table
+    HashTable hash;
+
+    //display table 
+    hash.displayTable();
+
+    //insert 
+    cout << "INSERTING 2, 3, 4\n";
+    hash.insertValue(2);
+    hash.insertValue(3);
+    hash.insertValue(4);
+
+    //display table after insert
+    hash.displayTable();
+
+    //remove one
+    cout << "REMOVING 3\n";
+    hash.deleteValue(3);
+
+    //display table after remove
+    hash.displayTable();
+
+    //search for key in table
+    hash.searchValue(4);
+    hash.searchValue(3);
+    
     return 0;
 }
--- linearhash.h
+++ linearhash.h
@@ -4,6 +4,7 @@
 #include<iostream>
 using std::cout;
 using std::cin;
+const int TableSize =  10;
 
 //create hash table class
 class HashTable
@@ -11,11 +12,107 @@ class HashTable
 private:
     int* table;
     int size;
+    int track;
+
 public:
     HashTable()
     {
-        
+        size = TableSize;
+        table = new int[size];
+        track = 0;
+
+        //initialize all empty slots
+        for(int i = 0; i < size; i++)
+        {
+            table[i] = -1;
+        }
     }
+
+    //Hash function key
+    int hashFunction(int key)
+    {
+        return key % size;
+    }
+
+    //check if hash table is full 
+    bool checkIfFull()
+    {
+        if(track == size)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    //Insert value into hash table 
+    void insertValue(int key)
+    {
+        if(checkIfFull() == false)
+        {
+            return;
+        }
+
+        int index = hashFunction(key);
+
+        while(table[index] != -1)        //iterate through the table until we find an empty slot
+        {
+            index = (index + 1) % size;
+        }
+
+        table[index] = key;
+        track++;
+    }
+
+    //Delete value from hash table
+    void deleteValue(int key)
+    {
+        int index = hashFunction(key);
+
+        while(table[index] != -1)
+        {
+            if(table[index] == key)
+            {
+                table[index] = -1;
+                return;
+            }
+            index = (index + 1) % size;
+        }
+    }
+
+    //Search value in hash table
+    void searchValue(int key)
+    {
+        int index = hashFunction(key);
+
+        /*linear probing 
+        (works because the key has to iterate through the same path once guaranteeing that table is not -1)*/
+        while(table[index] != -1)
+        {
+            if(table[index] == key)
+            {
+                cout << "Found Key " << key << " at index " << index << "\n"; 
+                return;
+            }
+            index = (index + 1) % size;
+        }
+        cout << "Key: " << key << " NOT FOUND\n";
+        return;
+    }
+
+    //Display hash table
+    void displayTable()
+    {
+        cout << "--- HASH TABLE CONTENTS ---\n" ;
+        for(int i = 0; i < size; i++)
+        {
+            cout << "INDEX: " << i << " KEY: " << table[i] << "\n";
+        }
+        cout << "\n";
+    }
+
 };
 
 #endif
