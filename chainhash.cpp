8026b1dfaff32b01ef550789a1f5cfdb75b9ee8e
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-06-03 11:47:56 +0900

    Complete hash table

--- chainhash.cpp
+++ chainhash.cpp
@@ -1,7 +1,73 @@
 #include <iostream>
-#include <chainhash.h>
+#include "chainhash.h"
 
 int main()
 {
+    int tableSize;
+    bool track = true;
+    cout << "--Chained Hashing-- \n" 
+    << "What is the Hash Table size? ";
+    cin >> tableSize;
+    cout << "\n";
+
+    //generate hashtable
+    HashTable hash(tableSize);
+
+    //display hash
+    cout << "--Table--\n";
+    hash.displayTable();
+
+    //give user interactive platform 
+    while(track == true)
+    {
+        int key;
+        int option;
+        cout << "Press 1:display 2:insert 3:remove 4:search 5:exit \n" << 
+        "Option: ";
+        cin >> option;
+        cout << "\n";
+        switch(option)
+        {
+            case 1:
+                cout << "--DISPLAY TABLE--\n";
+                hash.displayTable();
+                break;
+
+            case 2:
+                cout << "--INSERT--\n" << "Key: ";
+                cin >> key;
+                cout << "\n";
+                hash.insert(key);
+                break;
+
+            case 3:
+                cout << "--REMOVE--\n" << "Key: ";
+                cin >> key;
+                cout << "\n";
+                hash.remove(key);
+                break;
+
+            case 4:
+                cout << "--SEARCH--\n" << "Key: ";
+                cin >> key;
+                cout << "\n";
+                hash.search(key);
+                break;
+
+            case 5:
+                cout << "Exiting...\n";
+                track = false;
+                break;
+
+            default:
+                cout << "Please choose one of the options \n"
+                << "Options: 1:display 2:insert 3:remove 4:search 5:exit \n";
+                break;
+
+        }
+
+    }
+
+
     return 0;
 }
--- chainhash.h
+++ chainhash.h
@@ -2,10 +2,134 @@
 #define CHAINHASH
 
 #include <iostream>
+#include <vector>
 using std::cout;
 using std::cin;
+using std::vector;
+typedef struct _NODE
+{
+    int data;
+    struct _NODE *next;
+}NODE;
 
+class HashTable
+{
+private:
+    vector<NODE*> table;
+    int size;
+public:
+    HashTable(int tableSize)
+    {
+        size = tableSize;
+        table.resize(size, nullptr);
+    }
+    
+    //hash key function
+    int hashFunction(int key)
+    {
+        return key % size;
+    }
 
+    //Insert key
+    void insert(int key)
+    {
+        int index = hashFunction(key);
+        NODE* node = new NODE;
+        node->data = key;
+        node->next = nullptr;
+
+        if (table[index] == nullptr)
+        {
+            table[index] = node;
+        }
+        else
+        {
+            NODE* current = table[index];
+            while(current->next != nullptr)
+            {
+                current = current->next;
+            }
+
+            current->next = node;
+        }
+    }
+
+    //Remove key
+    void remove(int key)
+    {
+        int index = hashFunction(key);
+        
+        //table is empty
+        if(table[index] == nullptr)
+        {
+            return;
+        }
+
+        NODE* current = table[index];
+        NODE* previous = current;
+        //deleting first node
+        if(current->data == key) 
+        {
+            current = current->next;
+            previous->next = nullptr;
+            table[index] = nullptr;
+            table[index] = current;
+            return;
+
+        }
+
+        while (current->data != key && current->next != nullptr)
+        {
+            previous = current;
+            current = current->next;
+        }
+        if(current->data == key)
+        {
+            previous->next = current->next;
+            current->next = nullptr;
+            current = previous->next;
+        }
+
+    }
+
+    //Search key
+    void search(int key)
+    {
+        int index = hashFunction(key);
+
+        NODE* current = table[index];
+
+        while(current != nullptr)
+        {
+            if(current->data == key)
+            {
+                cout << "Key: " << key << " Found\n";
+                return;
+            }
+            current = current->next;
+        }
+
+        cout << "Key: " << key << " Not Found\n";
+
+    }
+
+    //Display table
+    void displayTable()
+    {
+        for(int i = 0; i < size; i++)
+        {
+            NODE* current = table[i];
+            cout << "INDEX " << i << ":";
+            while(current != nullptr)
+            {
+                cout << " -> " << current->data;
+                current = current->next;
+            }
+            cout << "\n";
+        }
+    }
+
+};
 
 
 #endif
