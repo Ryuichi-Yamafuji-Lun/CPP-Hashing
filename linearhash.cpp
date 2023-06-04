d986a3949a02385dae55738430ab9e98569e5e38
Ryuichi Lun <ryuichi.y.lun@gmail.com>
2023-06-04 11:41:31 +0900

    Added user interaction to LP hash table

--- linearhash.cpp
+++ linearhash.cpp
@@ -6,28 +6,52 @@ int main()
     //create Hash table
     HashTable hash;
 
+    cout << "--Linear Probing Hash Table--\n";
     //display table 
     hash.displayTable();
 
-    //insert 
-    cout << "INSERTING 2, 3, 4\n";
-    hash.insertValue(2);
-    hash.insertValue(3);
-    hash.insertValue(4);
-
-    //display table after insert
-    hash.displayTable();
-
-    //remove one
-    cout << "REMOVING 3\n";
-    hash.deleteValue(3);
-
-    //display table after remove
-    hash.displayTable();
-
-    //search for key in table
-    hash.searchValue(4);
-    hash.searchValue(3);
+    //user interaction
+    bool x = true;
+    int option;
+    while(x == true)
+    {
+        int key;
+        cout << "Options: 1.insert 2.delete 3.search 4.display 5.exit\n" << "Input: ";
+        cin >> option;
+        cout << "\n";
+        switch(option)
+        {
+            case 1:
+                cout << "--Insert--\n" << "Insert key: ";
+                cin >> key;
+                cout << "\n";
+                hash.insertValue(key);
+                break;
+            case 2:
+                cout << "--Delete--\n" << "Delete key: ";
+                cin >> key;
+                cout << "\n";
+                hash.deleteValue(key);
+                break;
+            case 3:
+                cout << "--Search--\n" << "Search key: ";
+                cin >> key;
+                cout << "\n";
+                hash.searchValue(key);
+                break;
+            case 4:
+                cout << "--Display Table--\n";
+                hash.displayTable();
+                break;
+            case 5:
+                cout << "Exiting...\n";
+                x = false;
+                break;
+            default:
+                cout << "Choose between option 1.insert 2.delete 3.search 4.display 5.exit\n";
+                break;
+        }
+    }
     
     return 0;
 }
